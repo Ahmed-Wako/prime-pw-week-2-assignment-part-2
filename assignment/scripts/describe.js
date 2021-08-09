28d57c76d825b02caddfb941c91e1d6e80fcdddd
Ahmed Wako <ahmed.h.wako@gmail.com>
2021-08-08 22:39:48 -0500

    Description 1 completed

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,9 +26,9 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// We make a variable called name and set it to 'Dane' as a string.
+// we check if the name is equal to 'Mary' and if it is, we would log 'Hi Mary' or else we will log 'how do you do?'
+// since the correct name is 'Dane', the log would show 'how do you do?'
 //
 
 //CODE
